658abbb0d2355a49a152c2cd71780873fa5e6bec
Sal-r <53383994+Sal-r@users.noreply.github.com>
2020-05-21 19:00:50 -0400

    auto-filter
    part 1 complete :)
    used this:
    https://www.w3schools.com/howto/howto_js_filter_table.asp

--- level 1/static/js/app.js
+++ level 1/static/js/app.js
@@ -2,3 +2,37 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+
+var tbody = d3.select("tbody");
+
+console.log(data);
+
+data.forEach((sighting) => {
+  var row = tbody.append("tr");
+  Object.entries(sighting).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
+
+function filterFunc() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("datetime");
+  filter = input.value;
+  table = document.getElementById("ufo-table");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
